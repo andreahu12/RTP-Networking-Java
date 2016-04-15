c278cfb065cdde794304d60c93363c0074272a33
Andrea Chi-en Hu <ahu35@gatech.edu>
2016-04-15 01:54:01 -0400

    Parses commandline arguments in ftaclient.

--- Part1/src/ftaclient.java
+++ Part1/src/ftaclient.java
@@ -2,6 +2,16 @@
 public class ftaclient {
 
 	public static void main(String[] args) {
+		if (args.length != 2) {
+			throw new Exception("ftaclient args should be: <Server Host IP or Name>:<Server Port> <Receive Window In Bytes");
+		}
+		
+		String serverIPorName = args[0];
+		int serverPort = Integer.valueOf(args[1]);
+		int receiveWindowSize = Integer.valueOf(args[2]);
+		
+		
+		
 		// java ftaclient H:P W
 			// H: IP Address OR Host Name of ftaserver
 			// P: UDP Port Number of ftaserver
